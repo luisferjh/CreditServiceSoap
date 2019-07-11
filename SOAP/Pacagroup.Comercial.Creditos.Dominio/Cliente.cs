9fdfc10578f10d980a9066e8569d11d7e7d70f22
luifer227 <luisjimenezh7@gmail.com>
2019-07-11 11:33:43 -0500

    Created the project architecture, Added Application, Infrastructure and domain Layer, Created Cliente and Credito Entity on Dominio layer. finally added implementacion, contrato and Wcfservice project on Application layer

---
+++ SOAP/Pacagroup.Comercial.Creditos.Dominio/Cliente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pacagroup.Comercial.Creditos.Dominio
+{
+    [DataContract]
+    public class Cliente
+    {
+        [DataMember]
+        public int IdCliente { get; set; }
+
+        [DataMember]
+        public string Nombre { get; set; }
+
+        [DataMember]
+        public string ApellidoPaterno { get; set; }
+
+        [DataMember]
+        public string ApellidoMaterno { get; set; }
+
+        [DataMember]
+        public string TipoDocumento { get; set; }
+
+        [DataMember]
+        public string NumeroDocumento { get; set; }
+
+        [DataMember]
+        public string Sexo { get; set; }
+
+        [DataMember]
+        public DateTime FechaNacimiento { get; set; }
+
+        [DataMember]
+        public string Dirrecion { get; set; }
+
+        [DataMember]
+        public string CodigoPostal { get; set; }
+
+        [DataMember]
+        public string EstadoCivil { get; set; }
+    }
+}
---
+++ SOAP/Pacagroup.Comercial.Creditos.Implementacion/ClienteService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Pacagroup.Comercial.Creditos.Contrato;
+using Pacagroup.Comercial.Creditos.Dominio;
+
+
+namespace Pacagroup.Comercial.Creditos.Implementacion
+{
+    public class ClienteService:IClienteService
+    {
+        public Cliente ObtenerCliente(string NumeroDocumento)
+        {
+            return null;
+        }
+
+
+        public IEnumerable<Cliente> ListarCliente()
+        {
+            return null;
+        }
+      
+    }
+}
